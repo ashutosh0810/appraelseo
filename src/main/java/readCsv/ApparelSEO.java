d9a9fca678ac4a07b0a05f7bc9bea9945930ba9b
ashutosh0810 <39034846+ashutosh0810@users.noreply.github.com>
2023-02-17 02:02:04 +0530

    Update ApparelSEO.java
    apparel

--- src/main/java/readCsv/ApparelSEO.java
+++ src/main/java/readCsv/ApparelSEO.java
@@ -1,10 +1,13 @@
 package readCsv;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ApparelSEO {
     public static boolean flag = true;
 
     public static void main(String[] args) {
-       //log.info(" Starting the Apparel search engine optimisation ");
+        log.info(" Starting the Apparel search engine optimisation ");
         FlowEngine flowEngine = new FlowEngine();
         flowEngine.start();
     }
